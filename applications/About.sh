27880de01990e608414b2090cd8a8a5094b35185
Yuvraj Tiwary <156575870+yuvrajtiwary-bitmesraece@users.noreply.github.com>
2024-08-25 19:42:27 +0530

    Yoyo

--- applications/About.sh
+++ applications/About.sh
@@ -3,9 +3,8 @@ cat <<EOF >~/.local/share/applications/About.desktop
 Version=1.0
 Name=About
 Comment=System information from Fastfetch
-Exec=alacritty --config-file /home/$USER/.local/share/omakub/defaults/alacritty/pane.toml --class=About --title=About -e bash -c 'fastfetch; read -n 1 -s'
+Exec=alacritty --config-file /home/$USER/.local/share/omakub/defaults/alacritty/pane.toml -e bash -c 'fastfetch; read -n 1 -s'
 Terminal=false
-# single list
 Type=Application
 Icon=/home/$USER/.local/share/omakub/applications/icons/Ubuntu.png
 Categories=GTK;
